6f171dfecda62b64fb815a5ab15e3a2fbb05f62d
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-10-09 11:20:37 +0700

    [UPDATED] Format document at routers users

--- routers/users.js
+++ routers/users.js
@@ -7,20 +7,20 @@ const checkJwt = usersController._checkJwt
 //ENDPOINT USERS
 //Show all users
 router.get('/users', usersController._getAllUsers)
-  
-  //Register User
-  router.post('/users/register', usersController._inputValidation, usersController._registUser)
-  
-  //Login User
-  router.post('/users/login', usersController._loginUser)
-  
-  //Profile User
-  router.get('/users/me', checkJwt, usersController._profileUser)
-  
-  //Edit User
-  router.put('/users/edit', checkJwt, usersController._inputValidationProf, usersController._editUser)
-  
-  //Edit Password User
-  router.put('/users/edit/password', checkJwt, usersController._inputValidationPass, usersController._editPass)
 
-  module.exports = router;
+//Register User
+router.post('/users/register', usersController._inputValidation, usersController._registUser)
+
+//Login User
+router.post('/users/login', usersController._loginUser)
+
+//Profile User
+router.get('/users/me', checkJwt, usersController._profileUser)
+
+//Edit User
+router.put('/users/edit', checkJwt, usersController._inputValidationProf, usersController._editUser)
+
+//Edit Password User
+router.put('/users/edit/password', checkJwt, usersController._inputValidationPass, usersController._editPass)
+
+module.exports = router;
