18ff2dbac606a9cd1ce5d51eab7050c5146aa117
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-10-09 11:23:01 +0700

    [UPDATED] Format document models users

--- models/users.js
+++ models/users.js
@@ -1,55 +1,55 @@
 const database = require("../database")
 
 const modelUsers = {
-    getAllUsers: async () => {
-        const request = await database`SELECT first_name, last_name, phone_number, photo_profile FROM users`;
+  getAllUsers: async () => {
+    const request = await database`SELECT first_name, last_name, phone_number, photo_profile FROM users`;
 
-        return request
-    },
-    checkEmail: async (email) => {
-        const checkEmail = await database`SELECT * FROM users WHERE email = ${email}`;
+    return request
+  },
+  checkEmail: async (email) => {
+    const checkEmail = await database`SELECT * FROM users WHERE email = ${email}`;
 
-        return checkEmail
-    },
-    registUser: async (payload) => {
-        const {
-            first_name,
-            last_name,
-            phone_number,
-            email,
-            hash,
-            photo_profile,
-        } = payload
-        const request = await database`INSERT INTO users
+    return checkEmail
+  },
+  registUser: async (payload) => {
+    const {
+      first_name,
+      last_name,
+      phone_number,
+      email,
+      hash,
+      photo_profile,
+    } = payload
+    const request = await database`INSERT INTO users
         (first_name, last_name, phone_number, email, password, photo_profile)
       values
         (${first_name}, ${last_name}, ${phone_number}, ${email}, ${hash}, ${photo_profile}) RETURNING id`
 
-        return request
-    },
-    profileUser: async (id) => {
-        const request = await database`SELECT * FROM users WHERE id = ${id}`
+    return request
+  },
+  profileUser: async (id) => {
+    const request = await database`SELECT * FROM users WHERE id = ${id}`
 
-        return request
-    },
-    editProfile: async (reqBody, coloumns, id) => {
-        const request = await database`
+    return request
+  },
+  editProfile: async (reqBody, coloumns, id) => {
+    const request = await database`
       UPDATE users SET ${database(
-        reqBody,
-        coloumns
-      )} WHERE id = ${id} RETURNING id`
+      reqBody,
+      coloumns
+    )} WHERE id = ${id} RETURNING id`
 
-      return request;
-    },
-    editPass: async (pass, coloumns, id) => {
-        const request = await database`
+    return request;
+  },
+  editPass: async (pass, coloumns, id) => {
+    const request = await database`
       UPDATE users SET ${database(
-        pass,
-        coloumns
-      )} WHERE id = ${id} RETURNING id`
+      pass,
+      coloumns
+    )} WHERE id = ${id} RETURNING id`
 
-      return request;
-    }
+    return request;
+  }
 }
 
 module.exports = modelUsers;
