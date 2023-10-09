0bcdbb0a5c9cd0b97cf3fded744f541abd06d19d
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-10-09 11:21:12 +0700

    [UPDATED] Format document routers movies

--- routers/movies.js
+++ routers/movies.js
@@ -5,17 +5,17 @@ const moviesController = require("../controllers/movies")
 //ENDPOINT MOVIES
 //Show all movies
 router.get('/movies', moviesController._getAllMovies);
-  
-  //Show selected movie
-  router.get('/movies/:id', moviesController._getSelectedMovie);
-  
-  //Add data movie
-  router.post("/movies", moviesController._inputValidation, moviesController._addMovie)
-  
-  //Edit data movie
-  router.put("/movies/:id", moviesController._inputValidation, moviesController._editMovie)
-  
-  //Delete Movie
-  router.delete("/movies/:id", moviesController._deleteMovie)
 
-  module.exports = router;
+//Show selected movie
+router.get('/movies/:id', moviesController._getSelectedMovie);
+
+//Add data movie
+router.post("/movies", moviesController._inputValidation, moviesController._addMovie)
+
+//Edit data movie
+router.put("/movies/:id", moviesController._inputValidation, moviesController._editMovie)
+
+//Delete Movie
+router.delete("/movies/:id", moviesController._deleteMovie)
+
+module.exports = router;
